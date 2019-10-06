3bc54ab290333b718d4b171949f7ec7a9381eedf
shonjiho <jihoson94@gmail.com>
2019-10-06 15:28:42 +0900

    using node-fetch, GraphQL connect with rest API

--- graphql/db.js
+++ graphql/db.js
@@ -1,39 +1,8 @@
-let movies = [
-  { id: 0, name: "Joket", score: 1.4 },
-  { id: 1, name: "sdf", score: 1.4 },
-  { id: 2, name: "asdsa", score: 3.4 },
-  { id: 3, name: "asdf", score: 4.4 },
-  { id: 4, name: "Joasdfaket", score: 2.2 },
+import fetch from "node-fetch";
 
-  { id: 5, name: "Jokadf sdfet", score: 5.4 },
-  { id: 6, name: "Joasdfafket", score: 6.4 },
-  { id: 7, name: "J  sfoket", score: 8.4 },
-  { id: 8, name: "Josdf  ket", score: 2.4 },
-  { id: 9, name: "Jsdf  oket", score: 4.4 }
-];
-export const getMovies = () => movies;
+const API_URL = "https://yts-proxy.now.sh/list_movies.json";
 
-export const getById = id => {
-  const filteredMoives = movies.filter(movie => id === movie.id);
-  return filteredMoives[0];
-};
-
-export const deleteMovie = id => {
-  const cleanedMovie = movies.filter(movie => id !== movie.id);
-  if (movies.length > cleanedMovie.length) {
-    movies = cleanedMovie;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
+export const getMovies = (limit, rating) =>
+  fetch(API_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies);
